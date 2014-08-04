a9ffe12ebe6532de96d888f6bcba3ec74bf8a558
janceChun <756837919@qq.com>
2014-08-04 17:04:20 +0800

    add  login  info

--- WebProject/serverPro/app/controllers/Application.java
+++ WebProject/serverPro/app/controllers/Application.java
@@ -1,21 +1,64 @@
 package controllers;
-
-import play.*;
+import models.Login;
+import net.sf.json.JSONObject;
+import play.Logger;
+import play.cache.Cache;
 import play.mvc.*;
 
-import java.util.*;
-
-//import models.*;
+import java.util.HashMap;
 
 public class Application extends Controller {
 
     public static void index() {
+//       System.out.print(session.get("userId")+"\n");
         redirect("../../public/index.html");
-//        render();
-    }
 
-    public static void login(){
+    }
+    public static void login() {
         String userName = params.get("userName");
-        renderText("userName receive: " + userName);
+        String password=params.get("password");
+        Login  userLogin =new Login();
+        Integer u_id;
+        u_id= userLogin.UserLogin(userName, password);
+        if (u_id==0){
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("code", "1");
+            jsonObject.put("message", "登录失败，账号密码不存在");
+            String res = jsonObject.toString();
+            renderJSON(res);
+        }else{
+//            SetCookie setCookie=new SetCookie();
+//            HashMap scache = new HashMap();
+//            scache.put("user",u_id);
+//            scache.put("sessionID", session.getId());
+//            Cache.set("session_"+session.getId(), scache, "30mn");//30 mins session timeout
+//            Logger.debug("user " + u_id + " logged in as session " + session.getId());
+
+
+//            Cache.set(session.getId(), u_id);
+            session.put("userId",u_id);
+//            String userIdSession=session.getId();
+//            System.out.print("\n"+"session"+userIdSession+"\n");
+//                    renderText("userName receive: " + userName);
+//            renderJSON("{\"code\": " + "0"+ "\"message\":"+"登录成功"+"}");
+//            renderJSON("");
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("code", "0");
+            jsonObject.put("msg", "登录成功");
+            jsonObject.put("data", "success");
+            String res = jsonObject.toString();
+            renderJSON(res);
+
+        }
+
+
+
+
+    }
+    public  static  void  addShopInfo(){
+        String u_id;
+        u_id=session.get("userId");
+        renderText("u_id:"+u_id);
     }
 }
--- WebProject/serverPro/public/javascripts/login.js
+++ WebProject/serverPro/public/javascripts/login.js
@@ -1,22 +1,54 @@
+ var HOST="http://"+window.location.host;
 function login(){
   $("#loginBut").live('click',function(){
    var login_username = $("#username").val();
    var login_password=$("#password").val();
-     var host="http://"+window.location.host;
-
+   if(!login_username||!login_password){
+     alert("请填入账号或密码")
+     return
+   }
       $.ajax({
-            url:  host + "/login",
+            url:  HOST + "/login",
             type: "POST",
+            dataType:"JSON",
             data:{ "userName":login_username,"password":login_password},
             success: function(data){
-//             alert(data)
+            if(data["code"]==0){
+              window.location.href="adminIndex.html"
+            }else{
+               alert(data["message"])
+            }
                 console.log(data);
             },
             error: function(data){
+            alert("网络故障，请重新操作")
                 console.log(data);
             }
         });
   })
+//  var get_cookie=getCookie("userId");
+//  alert(get_cookie)
 
 }
 login()
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=")
+  if (c_start!=-1)
+    {
+    c_start=c_start + c_name.length+1;
+    c_end=document.cookie.indexOf(";",c_start);
+    if (c_end==-1) c_end=document.cookie.length;
+    var tempResult=unescape(document.cookie.substring(c_start,c_end));
+    var len=tempResult.length
+   tempResult=tempResult.substring(0, len-1)
+    return  tempResult
+
+
+    }
+  }
+return ""
+}
+
