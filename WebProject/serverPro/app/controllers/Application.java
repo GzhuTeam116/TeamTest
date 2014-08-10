79bc037ae3f3f995b83ecbb554bf10d91d190774
SineTitans <565022817@qq.com>
2014-08-10 21:49:44 +0800

    加了个用于向数据库写入uuid的debug API，将来可能正式加入到项目中

--- WebProject/serverPro/app/controllers/Application.java
+++ WebProject/serverPro/app/controllers/Application.java
@@ -100,7 +100,7 @@ public class Application extends Controller  {
             ans.put("msg", "未能寻找到路径！");
             Boolean IS_LAN = JudgeLan.JudgeLan(request.remoteAddress);
             ans.put("access_method", IS_LAN ? "local":"remote");
-            renderJSON(ans);
+            renderJSON(ans.toString());
         }
     }
 
@@ -147,7 +147,7 @@ public class Application extends Controller  {
             info.put("area_discount", Regional.RegionalDiscount(ans.getInt("tid")));
             
             jsonRet.put("area_info", info);
-            renderJSON(jsonRet);
+            renderJSON(jsonRet.toString());
         } catch (SQLException ex) {
             Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
             JSONObject jsonRet = new JSONObject();
@@ -155,7 +155,7 @@ public class Application extends Controller  {
             jsonRet.put("msg", "未能查到该区域！");
             Boolean IS_LAN = JudgeLan.JudgeLan(request.remoteAddress);
             jsonRet.put("access_method", IS_LAN ? "local":"remote");
-            renderJSON(jsonRet);
+            renderJSON(jsonRet.toString());
         }
     }
 }
