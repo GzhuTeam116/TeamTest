1752e709481cb6e64163690c0b8b8a7ec2d38a32
janceChun <756837919@qq.com>
2014-08-19 14:27:54 +0800

    add userRegister

--- WebProject/serverPro/app/controllers/UserApi.java
+++ WebProject/serverPro/app/controllers/UserApi.java
@@ -1,9 +1,15 @@
 package controllers;
 
 import models.JudgeLan;
+import models.SqlConnect;
 import net.sf.json.JSONObject;
+import play.db.DB;
 import play.mvc.Controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static models.Login.RegisterResultApi;
 import static models.Shopping.addShopingcart;
 import static models.Shopping.getShopping;
 
@@ -24,6 +30,8 @@ public class UserApi extends Controller {
         returnJson.put("code","0");
         returnJson.put("msg","obtain shopcart msg");
         returnJson.put("data",dateJson);
+        Boolean IS_LAN = JudgeLan.JudgeLan(request.remoteAddress);
+        returnJson.put("access_method", IS_LAN ? "local":"remote");
         renderJSON(returnJson);
 
     }
@@ -56,6 +64,66 @@ public class UserApi extends Controller {
             obj.put("data",obj2);
             renderJSON(obj.toString());
 
+        }
+    }
+ //用户登录
+    public  static  void VerifyLogin() throws SQLException {
+        String userName=params.get("userName");
+        String  password =params.get("password");
+        SqlConnect sqlconn=new SqlConnect(DB.getConnection());
+        String sql="select tid from t_user where account='"+userName+"' and password='"+password+"'";
+        ResultSet res=sqlconn.Query(sql);
+        String loginUser=session.get("userId");
+        int loginUserId= Integer.parseInt(loginUser);
+        Boolean IS_LAN = JudgeLan.JudgeLan(request.remoteAddress);
+        JSONObject obj=new JSONObject();
+        obj.put("access_method",IS_LAN?"local":"remote");
+        while( res.next()){
+           int uerId=res.getInt("tid");
+            if(uerId==loginUserId){
+                obj.put("code",0);
+                obj.put("msg","verify");
+                obj.put("data","success");
+                renderJSON(obj);
+            }else{
+                obj.put("code",1);
+                obj.put("msg","verify");
+                obj.put("data","fail");
+                renderJSON(obj);
+            }
+        }
+    }
+//用户注册
+    public  static  void  RegisterResult() throws SQLException {
+        String userAccount=params.get("account");
+        String password=params.get("password");
+        String phone=params.get("phone");
+        String card_num=params.get("card_num");
+        String weibo_account=params.get("weibo_account");
+        Boolean IS_LAN = JudgeLan.JudgeLan(request.remoteAddress);
+        JSONObject obj=new JSONObject();
+        obj.put("access_method",IS_LAN?"local":"remote");
+        if(userAccount==""||password==""||phone==""){
+            obj.put("code","1");
+            obj.put("msg","register");
+            obj.put("data","faile");
+            System.out.print("\n"+"RegisterResult :"+"用户信息有误"+"\n");
+            renderJSON(obj);
+        }
+        int registerUserId=RegisterResultApi(userAccount,password, phone, card_num, weibo_account);
+        if(registerUserId==0){
+            obj.put("code","1");
+            obj.put("msg","register");
+            obj.put("data","faile");
+            System.out.print("\n"+"RegisterResult :"+"用户注册失败"+"\n");
+            renderJSON(obj);
+
+        }else {
+            obj.put("code","1");
+            obj.put("msg","register");
+            obj.put("data","success");
+            System.out.print("\n"+"RegisterResult :"+"用户注册成功"+"\n");
+            renderJSON(obj);
         }
     }
 }
--- WebProject/serverPro/app/models/Login.java
+++ WebProject/serverPro/app/models/Login.java
@@ -2,6 +2,9 @@ package models;
 
 import play.db.DB;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -15,7 +18,8 @@ public class Login {
     public static Integer UserLogin(String arg_account, String arg_password)  {
         try {
             SqlConnect sql = new SqlConnect(DB.getConnection());
-            String selectSql = "SELECT * FROM t_user where account='"+arg_account+"' and password= '"+arg_password+"' and is_admin=1";
+//            String selectSql = "SELECT * FROM t_user where account='"+arg_account+"' and password= '"+arg_password+"' and is_admin=1";
+            String selectSql = "SELECT * FROM t_user where account='"+arg_account+"' and password= '"+arg_password+"'";
             ResultSet selectRes = sql.Query(selectSql);
             while (selectRes.next()) {
                 String account = selectRes.getString("account");
@@ -29,5 +33,26 @@ public class Login {
             Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
         }
         return 0;
+    }
+
+    public  static  int RegisterResultApi(String arg_account,String arg_password,String arg_phone,String arg_carNum,String arg_weiBoAccount) throws SQLException {
+        int registerUserId=0;
+        Connection conn=DB.getConnection();
+       java.sql.Statement sts=null;
+        sts=conn.createStatement();
+        String insertSql="insert into t_user  (account,password,is_admin,phone,card_num,weibo_account) values (?,?,?,?,?,?)";
+        PreparedStatement pst=conn.prepareStatement(insertSql,sts.RETURN_GENERATED_KEYS);
+        pst.setString(1,arg_account);
+        pst.setString(2,arg_password);
+        pst.setString(3,"0");
+        pst.setString(4,arg_phone);
+        pst.setString(5,arg_carNum);
+        pst.setString(6,arg_weiBoAccount);
+        pst.execute();
+        ResultSet ret=pst.getGeneratedKeys();
+        if(ret!=null && ret.next()){
+            registerUserId=ret.getInt(1);
+        }
+        return registerUserId;
     }
  }
